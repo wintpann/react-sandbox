869f828bac554c019d62c899a46ec20cb2f42dc1
Sergey Manshin <sergey.manshin@devexperts.com>
2021-08-15 17:49:35 +0300

    Setup aliases

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,28 @@
 /* eslint-disable */
 const path = require('path');
+const { alias, configPaths } = require('react-app-rewire-alias')
 
-const entryMap = {
-    demo: path.resolve(__dirname, 'src', 'demo', 'one.tsx'),
-    export: path.resolve(__dirname, 'src', 'two.tsx'),
-};
-
-module.exports = function override(config) {
-    console.log('LOOOG here');
+const rewireEntries = (config) => {
+    const entryMap = {
+        demo: path.resolve(__dirname, 'src', 'build-scripts', 'demo.tsx'),
+        export: path.resolve(__dirname, 'src', 'build-scripts', 'export.ts'),
+    };
     const buildMode = process.env.BUILD_MODE || 'export';
-    const c = {...config}
-    c.entry = entryMap[buildMode];
-    return c;
-};
+
+    const currentEntry = entryMap[buildMode];
+    config.entry = currentEntry;
+
+    return config;
+}
+
+const rewirePaths = (config) => {
+    const customPathsFileName = path.resolve(__dirname, 'tsconfig.paths.json');
+    const rewire = alias(configPaths(customPathsFileName))
+    return rewire(config)
+}
+
+const pipe = (...modifiers) => (config) => modifiers.reduce((rewiredConfig, modifier) => modifier(rewiredConfig), config)
+
+const override = pipe(rewireEntries, rewirePaths)
+
+module.exports = override;
---
+++ src/build-scripts/demo-app/app.component.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { AppStyled } from '@demo-app/app.styled';
+
+export const App: FC = () => (
+    <AppStyled>
+        App
+    </AppStyled>
+);
